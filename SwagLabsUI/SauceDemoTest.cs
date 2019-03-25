44e7aaaaeb8242b6bc489f0e653fc85e5cbd082f
JokerJak <jakmaynard@gmail.com>
2019-03-24 23:02:38 -0600

    Refactor

--- SwagLabsUI/SauceDemoTest.cs
+++ SwagLabsUI/SauceDemoTest.cs
@@ -8,50 +8,55 @@ using SwagLabsUI.Pages;
 
 namespace SwagLabsUI
 {
-    [TestClass]
-    public class SauceDemoTest
+  [TestClass]
+  public class SauceDemoTest
+  {
+    LoginPage loginPage;
+    ProductPage productPage;
+    CartPage cartPage;
+    const string username = "standard_user";
+    const string password = "secret_sauce";
+    int expectedItems = 2;
+    const string onesie = "Sauce Labs Onesie";
+    const string bikelight = "Sauce Labs Bike Light";
+
+    [TestMethod]
+    public void AddItemsToCartValidateCartHasItems()
     {
-        LoginPage loginPage;
-        ProductPage productPage;
-        CartPage cartPage;
-        const string username = "standard_user";
-        const string password = "secret_sauce";
-        int expectedItems = 2;
-        const string onesie = "Sauce Labs Onesie";
-        const string bikelight = "Sauce Labs Bike Light";
+      //Setup Test 
+      var TestBrowser = new WebDriver();
+      loginPage = new LoginPage(TestBrowser);
+      productPage = new ProductPage(TestBrowser);
+      cartPage = new CartPage(TestBrowser);
 
-        [TestMethod]
-        public void AddItemsToCartValidateCartHasItems()
+      //Login
+      loginPage.LoadPage();
+      loginPage.InputUserName(username);
+      loginPage.InputPassword(password);
+      loginPage.ClickLogin();
+
+      //Add items to cart
+      productPage.AddItemToCart(bikelight);
+      productPage.AddItemToCart(onesie);
+      cartPage.GoToCart();
+
+      var cart = cartPage.GetItemsInCart();
+      Assert.IsNotNull(cart);
+      Assert.AreEqual(expectedItems, cart.Count, $"Expected 2 items in cart, there were {cart.Count}");
+
+      //Validate expected results
+      var expectedCartCount = 0;
+      foreach (var i in cart)
+      {
+        if (i.Name == onesie || i.Name == bikelight)
         {
-            var TestBrowser = new WebDriver();
-            loginPage = new LoginPage(TestBrowser);
-            productPage = new ProductPage(TestBrowser);
-            cartPage = new CartPage(TestBrowser);
-            loginPage.LoadPage();
-            loginPage.InputUserName(username);
-            loginPage.InputPassword(password);
-            loginPage.ClickLogin();
-
-
-            productPage.AddItemToCart(bikelight);
-            productPage.AddItemToCart(onesie);
-            cartPage.GoToCart();
-
-            var cart = cartPage.GetItemsInCart();
-            Assert.IsNotNull(cart);
-            Assert.AreEqual(2, cart.Count, $"Expected 2 items in cart, there were {cart.Count}");
-            var expectedCartCount = 0;
-            foreach (var i in cart)
-            {
-                if (i.Name == onesie || i.Name == bikelight)
-                {
-                    expectedCartCount++;
-                }
-            }
-
-            Assert.AreEqual(expectedItems, expectedCartCount);
-
-            TestBrowser.Close();
+          expectedCartCount++;
         }
+      }
+
+      Assert.AreEqual(expectedItems, expectedCartCount);
+
+      TestBrowser.Close();
     }
+  }
 }
